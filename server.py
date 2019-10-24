bcd21f8f07adc3cec7e0877bac3cecf255c98c86
Mehveş Altay <altaymehves@hotmail.com>
2019-10-24 18:26:22 +0300

    Test database connection with dummy data and display

--- server.py
+++ server.py
@@ -1,4 +1,8 @@
+from util import get_db_url, fill_with_dummy_data
+
 from flask import Flask
+
+import psycopg2 as db
 
 
 app = Flask(__name__)
@@ -6,7 +10,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def home_page():
-    return "Hello, world!"
+    fill_with_dummy_data(get_db_url())
+
+    with db.connect(get_db_url()) as conn:
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM DUMMY")
+        values = cur.fetchall()
+        cur.close()
+
+    values = [str(v[0]) for v in values]
+    return "<br/>".join(values)
 
 
 if __name__ == "__main__":
