35fdf936254d73051d8721b60e3b6cfc17710371
Mehveş Altay <altaymehves@hotmail.com>
2019-10-26 12:55:36 +0300

    Add working audio functionality

--- server.py
+++ server.py
@@ -22,6 +22,19 @@ def home_page():
     return "<br/>".join(values)
 
 
+@app.route("/audio_test")
+def audio_page():
+    """
+    Page for testing whether playing audio files served from static folder works correctly
+    :return:
+    """
+    return """
+    <audio controls>
+        <source src="static/test_audio.mp3" type="audio/mpeg">
+        Your browser does not support the audio element.
+    </audio>
+    """
+
 @app.route("/logo_test")
 def logo_page():
     """
