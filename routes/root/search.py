20f4e4d05254efafd4b0aa5339d7a3258eb71303
Mehveş Altay <altaymehves@hotmail.com>
2019-12-17 04:43:25 +0300

    Implement search

--- routes/root/search.py
+++ routes/root/search.py
@@ -1,17 +1,21 @@
-from flask import session, render_template, redirect, current_app
+from flask import render_template, request, session
 
+from models.podcast import Podcast
 from models.user import User
-from util import user_logged_in
 
 from routes.root import root_blueprint
+from util import user_logged_in
 
 
-@root_blueprint.route("/search")
+@root_blueprint.route("/search", methods=['GET'])
 def search():
     user = None
     if user_logged_in():
         user = User.get_from_id(session['user_id'])
 
-    # todo: implement search page
+    args = request.args.to_dict()
+    q = args.get("q", None)
 
-    return
+    results = Podcast.search(q) if q else []
+
+    return render_template("root/search.html", user=user, q=q, results=results, page_title="Search")
